d9925feb8a6ac2028a4cf2aa7b88509dd939756a
Isaiah Gaudard <igaudard12@icloud.com>
2025-01-11 00:24:25 -0500

    Swerve Code

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class Constants {
+    public class OIConstants {
+        public static int kDriverPort = 0;
+        public static int kOperatorPort = 1;
+    }
+
+    public class ElevatorConstants {
+
+    }
+
+    public class FunnelConstants {
+
+    }
+
+    public class ClimberConstants {
+
+    }
+
+    public class CoralManipulatorConstants {
+
+    }
+}
